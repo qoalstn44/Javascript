72dc36da7a0553a3f14cafa8e3e185bb651eecf4
qoalstn44 <qoalstn44@naver.com>
2022-06-23 00:31:09 +0900

    Variables/constants

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt", "utf8")
 
@@ -18,5 +19,38 @@ console.log("hello, world!"); //이것은 단일행 주석입니다.
 어떤 문자가 와도 
 개행이 되어도 주석처리가 된다.
 console.log("hello, world!");
-*/
+
 console.log("hello, world!");
+
+let A = 123;
+console.log(A);
+
+A = 456;
+console.log(A)
+
+let A = 789; // Syntax error
+
+const B = 123; //상수 선언 및 값 초기화
+console.log(B) //outpot: 123
+
+const C;
+C = 123;
+*/
+//선언 후 할달
+let hi;
+hi = "holle";
+
+//선언과 동시에 초기화
+let halo = "holle!";
+
+//한 줄에 여러번 변수 선언하고 초기화
+let name = "john",
+    age = 13,
+    mag = "holle";
+
+//상수는 보통 대문자로 표기
+const TESTCASE = 5;
+const BIRTYDAY = "2020. 1. 1"
+
+console.log(age);
+console.log(BIRTYDAY);
