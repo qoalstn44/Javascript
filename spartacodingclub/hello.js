929d5ddb3074bedf12a07c811371b076aecfae8e
qoalstn44 <qoalstn44@naver.com>
2022-11-07 12:35:34 +0900

    New

---
+++ spartacodingclub/hello.js
@@ -0,0 +1,2 @@
+console.log('Hello World!!')
+// <--이것은 주석입니다.
---
+++ spartacodingclub/variable.js
@@ -0,0 +1,6 @@
+//let count = 1
+//const preIncrement = ++count
+//console.log(`const: ${count}, preIncrement: ${preIncrement}`)
+let count = 1
+const preIncrement = count++
+console.log(`const: ${count}, preIncrement: ${preIncrement}`)
