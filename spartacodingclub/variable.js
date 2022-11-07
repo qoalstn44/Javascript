7fe9f9dec97b283ac6d1e54a382c85500133c715
qoalstn44 <qoalstn44@naver.com>
2022-11-07 21:09:49 +0900

    연산자

--- spartacodingclub/variable.js
+++ spartacodingclub/variable.js
@@ -1,6 +1,8 @@
 //let count = 1
 //const preIncrement = ++count
 //console.log(`const: ${count}, preIncrement: ${preIncrement}`)
+
 let count = 1
 const preIncrement = count++
 console.log(`const: ${count}, preIncrement: ${preIncrement}`)
+
